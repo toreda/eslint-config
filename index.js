e51fc5cbd77f2671bd1a1deeefa4799460edc2ff
SaxanLosterToreda <70411010+SaxanLosterToreda@users.noreply.github.com>
2021-02-26 16:23:19 -0800

    more exceptions inside tests

--- index.js
+++ index.js
@@ -33,7 +33,8 @@ module.exports = {
 			rules: {
 				'@typescript-eslint/explicit-function-return-type': 0,
 				'@typescript-eslint/no-empty-function': 0,
-				'@typescript-eslint/no-explicit-any': 0
+				'@typescript-eslint/no-explicit-any': 0,
+				'@typescript-eslint/no-non-null-assertion': 0
 			}
 		}
 	]
