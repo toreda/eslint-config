8010ece018d4bfbf04ecda61d8e6d03cb5646bf4
Saxan Loster <70411010+SaxanLosterToreda@users.noreply.github.com>
2021-06-04 09:29:44 -0700

    Added workspace, expections for unused vars

--- index.js
+++ index.js
@@ -1,54 +1,57 @@
 module.exports = {
-	parser: "@typescript-eslint/parser",
+	parser: '@typescript-eslint/parser',
 	parserOptions: {
 		// Allows for the parsing of modern ECMAScript features
 		ecmaVersion: 2020,
 		// Allows for the use of imports
-		sourceType: "module",
-		ecmaFeatures: {}
+		sourceType: 'module',
+		ecmaFeatures: {},
 	},
 	settings: {},
 	extends: [
 		// Uses the recommended rules from the @typescript-eslint/eslint-plugin
-		"plugin:@typescript-eslint/recommended",
+		'plugin:@typescript-eslint/recommended',
 		// plugin-prettier and eslint-config-prettier. This will display prettier
 		// errors as ESLint errors. Make sure this is always the last configuration in the extends array.
-		"plugin:prettier/recommended",
+		'plugin:prettier/recommended',
 	],
 	rules: {
-		"@typescript-eslint/explicit-function-return-type": "error",
-		"@typescript-eslint/ban-ts-comment": "warn",
-		"@typescript-eslint/no-empty-interface": "warn",
-		"@typescript-eslint/no-explicit-any": "warn",
-		"@typescript-eslint/no-var-requires": "warn",
-		"@typescript-eslint/internal/no-typescript-default-import": "off",
-		"@typescript-eslint/no-inferrable-types": "off",
-		"max-len": ["warn", { code: 110 }],
+		'@typescript-eslint/ban-ts-comment': 'warn',
+		'@typescript-eslint/explicit-function-return-type': 'error',
+		'@typescript-eslint/internal/no-typescript-default-import': 'off',
+		'@typescript-eslint/no-empty-interface': 'warn',
+		'@typescript-eslint/no-explicit-any': 'warn',
+		'@typescript-eslint/no-inferrable-types': 'off',
+		'@typescript-eslint/no-namespace': 'off',
+		'@typescript-eslint/no-unused-vars': ['warn', {argsIgnorePattern: '^_', varsIgnorePattern: '^_'}],
+		'@typescript-eslint/no-var-requires': 'warn',
+		'@typescript-eslint/prefer-namespace-keyword': 'error',
+		'max-len': ['warn', {code: 110, ignoreStrings: true, ignoreTemplateLiterals: true, ignoreComments: true}],
 	},
 	overrides: [
 		{
-			files: ["*.spec.ts"],
+			files: ['*.spec.ts'],
 			rules: {
-				"@typescript-eslint/explicit-function-return-type": 0,
-				"@typescript-eslint/no-empty-function": 0,
-				"@typescript-eslint/no-explicit-any": 0,
-				"@typescript-eslint/no-non-null-assertion": 0,
-				'max-len': 0,
+				'@typescript-eslint/explicit-function-return-type': 'off',
+				'@typescript-eslint/no-empty-function': 'off',
+				'@typescript-eslint/no-explicit-any': 'off',
+				'@typescript-eslint/no-non-null-assertion': 'off',
+				'max-len': 'off',
 			},
 		},
 		{
 			files: ['./*.ts'],
 			rules: {
-				'@typescript-eslint/explicit-function-return-type': 0,
-				'@typescript-eslint/no-explicit-any': 0,
-				'@typescript-eslint/no-var-requires': 0,
+				'@typescript-eslint/explicit-function-return-type': 'off',
+				'@typescript-eslint/no-explicit-any': 'off',
+				'@typescript-eslint/no-var-requires': 'off',
 			},
 		},
 		{
-			files: ["*.js"],
+			files: ['*.js'],
 			rules: {
-				"@typescript-eslint/explicit-function-return-type": 0,
-				"@typescript-eslint/no-var-requires": 0,
+				'@typescript-eslint/explicit-function-return-type': 'off',
+				'@typescript-eslint/no-var-requires': 'off',
 			},
 		},
 	],
